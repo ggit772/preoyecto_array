7a51d182ba45c693c7b79396f6ffadf1c4b4af87
unknown <miemail@dominio.com>
2021-10-06 20:10:09 -0500

    array_actualizacion

--- js/envio_datos2.js
+++ js/envio_datos2.js
@@ -14,19 +14,29 @@ function agregar(){
 
     arreglo.push(persona);
 
-    if(arreglo.length >= 3){
-        arreglo.splice(arreglo.length-1,1);
-        alert("ingreso mas de 2");
-    }
+    // if(arreglo.length >= 3){
+    //     arreglo.splice(arreglo.length-1,1);
+    //     alert("ingreso mas de 2");
+    // }
 
-    document.getElementById('nombre').value="";
+    document.getElementById('nombre').value ="";
     document.getElementById('documento').value="";
     document.getElementById('telefono').value="";
 }
 
 function mostrar(){
-
+    
     console.log(arreglo);
+    console.log('orden: '+ org());
     alert(arreglo[1]['telefono']);
 }
 
+function org(t=[]){
+    //t =[];
+    for(var i=0 in arreglo){
+       
+        t[i]= arreglo[i]['nombre'];
+    }
+    t.sort();
+    return t;
+}
